90ee49a164fd5115b0cc34754eb2346eb267dca2
michelleheh <xiaoyu.he22@gmail.com>
2016-04-30 14:39:11 -0700

    Delete node_modules folder and add .gitignore
    Please enter the commit message for your changes. Lines starting

--- server/api/node_modules/nodal/cli/bin.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-const CLI = require('./cli.js');
-CLI.run(process.argv.slice(2));
--- server/api/node_modules/nodal/core/env.js
+++
@@ -1,8 +0,0 @@
-module.exports = (() => {
-
-  return {
-    name: process.env.NODE_ENV || 'development',
-    rootDirectory: process.cwd()
-  };
-
-})();
