c2a755320e800b0f78bad2833bd0cad5f7a4656c
Kevin Nguyen <account@kevinwin.com>
2016-04-30 17:57:38 -0700

    Add static file server w/ babel

---
+++ server/static/lib/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+let app = express();
+let port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/../public'));
+
+
+app.listen(port, () => console.log('Listening on port ' + port));
