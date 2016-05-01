d6e98353a044d0ad72987344f0b9e606e1187127
michelleheh <xiaoyu.he22@gmail.com>
2016-05-01 15:11:11 -0700

    Testing static server on AWS
    See: #5

--- server/static/server.js
+++ server/static/server.js
@@ -4,6 +4,9 @@ let port = process.env.port || 8000;
 
 app.use(express.static(__dirname + '/../public'));
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
 app.listen(port, function() {
   console.log('Listening on port 8000');
