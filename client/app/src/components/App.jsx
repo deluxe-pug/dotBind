8f9f4229078b1da8a9a063b4225c75d61c6b4c19
Connie Cheung <conniedcheung@gmail.com>
2016-05-02 18:48:40 -0700

    Add some MaterializeCSS styling but cannot see via webpack server

--- client/app/src/components/App.jsx
+++ client/app/src/components/App.jsx
@@ -3,13 +3,31 @@ import AddCardContainer from '../containers/AddCardContainer';
 import AddTagContainer from '../containers/AddTagContainer';
 import AllCardsContainer from '../containers/AllCardsContainer';
 import TagsContainer from '../containers/TagsContainer';
+import Sidebar from './Sidebar';
 
 const App = () => (
   <div>
-    <AllCardsContainer />
-    <AddCardContainer />
-    <TagsContainer />
-    <AddTagContainer />
+  
+    <header>
+      <div className="container">
+        <a href="#" 
+           data-activates="slide-out" 
+           className="button-collapse top-nav full hide-on-large-only">
+          <i className="material-icons">menu</i>
+        </a>
+      </div>
+      <div id="slide-out" className="side-nav fixed">
+        <Sidebar />
+      </div>
+    </header>
+
+    <main>
+      <AllCardsContainer />
+      <AddCardContainer />
+      <TagsContainer />
+      <AddTagContainer />
+    </main>
+
   </div>
 ); 
 
