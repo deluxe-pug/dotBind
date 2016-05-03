f8f4ebc104628fb5bac6f4cd1f87e4d6b3057194
Kevin Nguyen <account@kevinwin.com>
2016-05-03 13:19:26 -0700

    Add Search Bar Container

---
+++ client/app/src/containers/SearchContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchContainer = () => {
+  return (
+    <div className="navbar-fixed">
+        <div className="nav-wrapper">
+          <input type="search" placeholder="Search &#xF002;" style={{fontFamily: 'Arial, FontAwesome'}}/>
+        </div>
+    </div>
+  );
+};
+
+export default SearchContainer;
