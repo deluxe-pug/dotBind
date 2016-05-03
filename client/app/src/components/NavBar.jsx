867c5a61c12934b5ee1c206c607700f473794dcf
Kevin Nguyen <account@kevinwin.com>
2016-05-03 13:18:51 -0700

    Add Nav bar component
    See #26

---
+++ client/app/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchContainer from '../containers/SearchContainer';
+
+const NavBar = () => {
+  return (
+    <SearchContainer />
+  );
+
+};
+
+export default NavBar;
