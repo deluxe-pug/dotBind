e1035f3628c208739d6e0aaec7df943240b58c7f
Liam Hatcher <hatcherpiano@gmail.com>
2016-05-03 12:10:32 -0700

    Add tag renders to screen

--- client/app/src/containers/TagsContainer.jsx
+++ client/app/src/containers/TagsContainer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Tag from '../components/Tag';
 
-const TagsContainer = () => (
+const TagsContainer = ({tags}) => (
   <div>
-    TagsContainer
+    {tags.map( (tag) =>
+      <Tag key={tag.id}{...tag} /> 
+    )}
   </div>
 );
 
