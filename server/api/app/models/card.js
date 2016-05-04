edfec5b25ef325197d8a3fb851a04e36bbebc2e9
michelleheh <xiaoyu.he22@gmail.com>
2016-05-03 21:04:04 -0700

    Join card to card_tag to tag
    See: #9

--- server/api/app/models/card.js
+++ server/api/app/models/card.js
@@ -3,11 +3,14 @@ module.exports = (function() {
   'use strict';
 
   const Nodal = require('nodal');
+  const CardTag = Nodal.require('app/models/card_tag.js');
 
   class Card extends Nodal.Model {}
 
   Card.setDatabase(Nodal.require('db/main.js'));
   Card.setSchema(Nodal.my.Schema.models.Card);
+  
+  Card.joinedBy(CardTag, {multiple: true});
 
   return Card;
 
