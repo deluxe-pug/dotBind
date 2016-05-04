c3eebe90fd21388c9c056c98a53edcd37ac58c5e
michelleheh <xiaoyu.he22@gmail.com>
2016-05-03 19:11:44 -0700

    Refine database schema, add api end point tests
    See: #9

--- server/api/db/migrations/2016050203230180__create_cards.js
+++ server/api/db/migrations/2016050203230180__create_cards.js
@@ -15,10 +15,10 @@ module.exports = (function() {
 
       return [
         this.createTable("cards", [
-          {"name":"user_id","type":"int"},
           {"name":"icon", "type":"string"},
           {"name":"url", "type":"string"},
           {"name":"highlight", "type":"string"},
+          {"name":"user_id","type":"int","properties":{"unique":true}},
         ])
       ];
 
