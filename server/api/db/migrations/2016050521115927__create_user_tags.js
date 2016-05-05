8ab1a597c7632d55c06b75d25f09b8b94550ea12
Kevin Nguyen <account@kevinwin.com>
2016-05-05 14:33:31 -0700

    Add migrations for users and user_tags

---
+++ server/api/db/migrations/2016050521115927__create_user_tags.js
@@ -0,0 +1,34 @@
+module.exports = (function() {
+
+  "use strict";
+
+  const Nodal = require('nodal');
+
+  class CreateUserTags extends Nodal.Migration {
+
+    constructor(db) {
+      super(db);
+      this.id = 2016050521115927;
+    }
+
+    up() {
+
+      return [
+        this.createTable("user_tags", [{"name":"user_id","type":"int"},{"name":"tag_id","type":"int"}])
+      ];
+
+    }
+
+    down() {
+
+      return [
+        this.dropTable("user_tags")
+      ];
+
+    }
+
+  }
+
+  return CreateUserTags;
+
+})();
