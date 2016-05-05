062b887c97f6aae762bb7ad20e9bcb887d61d37c
Kevin Nguyen <account@kevinwin.com>
2016-05-05 15:46:42 -0700

    Add User and Tag join relationships

--- server/api/app/models/user_tag.js
+++ server/api/app/models/user_tag.js
@@ -4,10 +4,16 @@ module.exports = (function() {
 
   const Nodal = require('nodal');
 
+  const User = Nodal.require('app/models/user.js');
+  const Tag = Nodal.require('app/models/tag.js');
+
   class UserTag extends Nodal.Model {}
 
   UserTag.setDatabase(Nodal.require('db/main.js'));
   UserTag.setSchema(Nodal.my.Schema.models.UserTag);
+
+  UserTag.joinsTo(User, {multiple: true});
+  UserTag.joinsTo(Tag, {multiple: true});
 
   return UserTag;
 
