a9408860d7c5c823ed958d9897de5da7754866fe
Kevin Nguyen <account@kevinwin.com>
2016-05-05 14:35:09 -0700

    Add V1UsersController

---
+++ server/api/app/controllers/v1/users_controller.js
@@ -0,0 +1,66 @@
+module.exports = (function() {
+
+  'use strict';
+
+  const Nodal = require('nodal');
+  const User = Nodal.require('app/models/user.js');
+
+  class V1UsersController extends Nodal.Controller {
+
+    index() {
+
+      User.query()
+        .where(this.params.query)
+        .end((err, models) => {
+
+          this.respond(err || models);
+
+        });
+
+    }
+
+    show() {
+
+      User.find(this.params.route.id, (err, model) => {
+
+        this.respond(err || model);
+
+      });
+
+    }
+
+    create() {
+
+      User.create(this.params.body, (err, model) => {
+
+        this.respond(err || model);
+
+      });
+
+    }
+
+    update() {
+
+      User.update(this.params.route.id, this.params.body, (err, model) => {
+
+        this.respond(err || model);
+
+      });
+
+    }
+
+    destroy() {
+
+      User.destroy(this.params.route.id, (err, model) => {
+
+        this.respond(err || model);
+
+      });
+
+    }
+
+  }
+
+  return V1UsersController;
+
+})();
