2c1a63db4f84a25c59f542f7eb1408fc0b90b95c
Connie Cheung <conniedcheung@gmail.com>
2016-05-07 16:55:51 -0700

    Add filter using tags, need to return to initial state after

--- client/app/src/components/Tag.jsx
+++ client/app/src/components/Tag.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { filterCardsAction } from '../actions/cardActions';
 
-const Tag = (props) => {
-  return (
-    <div className="tag">
-        <span>#{props.name}</span>
-    </div>
-  );
+
+class Tag extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <a className="btn tag" onClick={() => this.props.filterCards(this.props.name)}>
+        {this.props.name}
+      </a>
+    );
+  }
 };
 
-export default Tag;
+// const mapStateToProps = (state) => {
+//   return {
+//     cards: state.cards
+//   };
+// }
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({filterCards: filterCardsAction}, dispatch);
+};
+
+export default connect(null, mapDispatchToProps)(Tag);
+
+// <div className="tag">
+//     <span>#{props.name}</span>
+// </div>
