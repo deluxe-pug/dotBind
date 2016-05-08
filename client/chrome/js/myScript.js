d5ca750cb26d79dde27271ef737ab9cf5cc6be50
michelleheh <xiaoyu.he22@gmail.com>
2016-05-07 17:07:09 -0700

    Update chrome myScript

--- client/chrome/js/myScript.js
+++ client/chrome/js/myScript.js
@@ -1,10 +1,13 @@
 (() => {
   console.log('myScript loaded!');
-
   const message = {
     from: 'myScript',
     selection: window.getSelection().toString(),
   };
 
   chrome.runtime.sendMessage(message, response => console.log(response.msg));
+
+  $('body').on('mouseup', () => {
+    $(window.getSelection().baseNode.parentNode).css({ color: 'red' });
+  });
 })();
