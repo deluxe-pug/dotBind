593b7f7518bb3fa263f7f0fcb61da30e85ff9a7e
Kevin Nguyen <account@kevinwin.com>
2016-05-10 12:27:37 -0700

    Add Access Token migration

---
+++ server/api/db/migrations/2016051002123395__create_access_tokens.js
@@ -0,0 +1,34 @@
+module.exports = (function() {
+
+  "use strict";
+
+  const Nodal = require('nodal');
+
+  class CreateAccessTokens extends Nodal.Migration {
+
+    constructor(db) {
+      super(db);
+      this.id = 2016051002123395;
+    }
+
+    up() {
+
+      return [
+        this.createTable("access_tokens", [{"name":"user_id","type":"int"},{"name":"access_token","type":"string"},{"name":"token_type","type":"string"},{"name":"expires_at","type":"datetime"},{"name":"ip_address","type":"string"}])
+      ];
+
+    }
+
+    down() {
+
+      return [
+        this.dropTable("access_tokens")
+      ];
+
+    }
+
+  }
+
+  return CreateAccessTokens;
+
+})();
