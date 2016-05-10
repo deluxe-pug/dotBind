c92c6d8eaa051bb13614de7db3e2cc4b112ffd2d
Kevin Nguyen <account@kevinwin.com>
2016-05-10 12:23:47 -0700

    Add access token

--- server/api/app/controllers/auth_controller.js
+++ server/api/app/controllers/auth_controller.js
@@ -4,12 +4,16 @@ module.exports = (function() {
 
   const Nodal = require('nodal');
 
+  const AccessToken = Nodal.require('app/models/access_token.js');
+
   class AuthController extends Nodal.Controller {
 
     authorize(callback) {
 
       this.setHeader('Cache-Control', 'no-store');
       this.setHeader('Pragma', 'no-cache');
+
+      AccessToken.verify(this.params, callback);
 
       callback(null);
 
