61604fc5931a8cd3277c82e29f09993a86564278
Kevin Nguyen <account@kevinwin.com>
2016-05-11 18:08:35 -0700

    Add auth endpoint

--- client/app/src/actions/endpoints.jsx
+++ client/app/src/actions/endpoints.jsx
@@ -2,6 +2,7 @@ const endpoints = {
   cards: 'http://localhost:3000/v1/cards',
   tags: 'http://localhost:3000/v1/tags',
   card_tags: 'http://localhost:3000/v1/card_tags',
+  card_tags: 'http://localhost:3000/v1/card_tags',
   auth: 'http://localhost:8000/auth'
 };
 
