5783d2f8fcc5a5402f61d0df685a3d3bfa8a94cb
Kevin Nguyen <account@kevinwin.com>
2016-05-11 18:56:39 -0700

    Add fetchUserAction

---
+++ client/app/src/actions/userActions.jsx
@@ -0,0 +1,16 @@
+// action creater - function that returns an object
+// object is automatically sent to all reducers
+// reducers will choose to return a different piece of state, depending on action
+// newly returned state piped into application state
+
+import axios from 'axios';
+import endpoints from './endpoints';
+
+export const fetchUserAction = () => {
+  const request = axios.get(endpoints.auth);
+  return {
+    type: 'FETCH_USER',
+    payload: request,
+  };
+};
+
