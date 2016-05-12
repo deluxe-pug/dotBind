5537ef8ca37b1de308c2ca527dac6f731f35a251
Kevin Nguyen <account@kevinwin.com>
2016-05-11 18:51:50 -0700

    Add userReducer

--- client/app/src/reducers/appReducer.jsx
+++ client/app/src/reducers/appReducer.jsx
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import cardsReducer from './cardsReducer';
 import tagsReducer from './tagsReducer';
+import userReducer from './userReducer';
 
 // mapping to application state
 // each key has each reducer, each piece of state
 const appReducer = combineReducers({
   cards: cardsReducer,
-  tags: tagsReducer
+  tags: tagsReducer,
+  user: userReducer,
 });
 
 export default appReducer;
