0807d9ac977883b24152f256a7e227be6a89e792
michelleheh <xiaoyu.he22@gmail.com>
2016-05-12 15:29:01 -0700

    Activiate background script to detect localstorage

--- client/chrome/js/myScript.js
+++ client/chrome/js/myScript.js
@@ -1,14 +1,32 @@
 (() => {
   console.log('myScript loaded!');
-  // form message to send to popup.js
-  const message = {
+  
+  // form message with selection to send to popup.js
+  const selection = {
     from: 'myScript',
-    selection: window.getSelection().toString(),
+    method: 'sendSelection',
+    selected: window.getSelection().toString(),
   };
-  // send mesage to popup.js
-  chrome.runtime.sendMessage(message, response => console.log(response.msg));
+  // form message with user infomation to send to background.js
+  const logInDotBind = {
+    from: 'myScript',
+    method: 'logInDotBind',
+    doBindAccesToken: localStorage.getItem('doBindAccesToken'),
+    gitHubId: localStorage.getItem('gitHubId'),
+  }
 
+  // send DotBind user infomation to background.js
+  if ( logInDotBind.doBindAccesToken ) {
+    chrome.runtime.sendMessage(logInDotBind, response => console.log(response.msg));
+  }
+  // send selected text to popup.js
+  if ( selection.selected ) {
+    chrome.runtime.sendMessage(selection, response => console.log(response.msg));
+  }
+  // Uncomment to highlight text when being clicked on
+  // Potential feature to add
   // $('body').on('mouseup', () => {
-  //   $(window.getSelection().baseNode.parentNode).css({ color: 'red' });
+  //   $(window.getSelection().baseNode.parentNode).css({ 'background-color': 'yellow' });
   // });
+
 })();
