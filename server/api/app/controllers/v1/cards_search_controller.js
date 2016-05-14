86c571a56fe1eeae77383eeac9e97dde86eea7aa
Connie Cheung <conniedcheung@gmail.com>
2016-05-13 21:13:34 -0700

    Set up barebones cards-search-controller

---
+++ server/api/app/controllers/v1/cards_search_controller.js
@@ -0,0 +1,37 @@
+module.exports = (function() {
+
+  'use strict';
+
+  const Nodal = require('nodal');
+  const PromiseMaker = require('bluebird').promisify;
+
+  class V1CardsSearchController extends Nodal.Controller {
+
+    index() {
+
+    }
+
+    show() {
+
+
+    }
+
+    create() {
+
+
+    }
+
+    update() {
+
+
+    }
+
+    destroy() {
+      
+    }
+
+  }
+
+  return V1CardsSearchController;
+
+})();
