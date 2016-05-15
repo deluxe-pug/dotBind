c5fe4366b0a38a571c7e5699b6929f45421a6d29
michelleheh <xiaoyu.he22@gmail.com>
2016-05-15 13:49:12 -0700

    Join card - message - user

--- server/api/app/models/message.js
+++ server/api/app/models/message.js
@@ -3,11 +3,16 @@ module.exports = (function() {
   'use strict';
 
   const Nodal = require('nodal');
+  const Card = Nodal.require('app/models/card.js');
+  const User = Nodal.require('app/models/user.js');
 
   class Message extends Nodal.Model {}
 
   Message.setDatabase(Nodal.require('db/main.js'));
   Message.setSchema(Nodal.my.Schema.models.Message);
+
+  Message.joinsTo(Card, {multiple: true});
+  Message.joinsTo(User, {multiple: true});
 
   return Message;
 
