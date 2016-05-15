63efda3292379e0145d8c71c146c41e0705f4ddf
Kevin Nguyen <account@kevinwin.com>
2016-05-14 20:29:39 -0700

    Generate access token for non-Github users

--- server/static/lib/regularAuth-handler.js
+++ server/static/lib/regularAuth-handler.js
@@ -8,20 +8,31 @@ exports.regularAuth = (req, res) => {
   const username = email.split('@')[0] // map to Github email if found in database [should also be done opposite direction] or 
 
   request({
-    uri: `http://localhost:3000/v1/access_tokens`
-  }, () => {
-
-  })
+    uri: 'http://localhost:3000/v1/access_tokens',
+    method: 'POST',
+    json: true,
+    body: {username, password, email}
+  }, (err, message, response) => {
+    if (err) {
+      console.error('Error POSTing to access_token: ', err);
+      return;
+    }
+    const access_token = username === 'public' ? 'dotBind' : response.data[0].access_token;
+    console.log('Your response from POSTing to access_tokens: ', response);
     // if the user is valid
       // set up session with username and token
-      cat.img((err, octoUrl) => {
-        req.session.dotBind = {};
-        req.session.dotBind.username = username;
-        req.session.dotBind.access_token = 'dotBind';
-        req.session.dotBind.img = octoUrl
-        // redirect to home page
-        res.redirect('/');
-      // if the user is not valid
-        // send back response 'user not valid'
-      }, true);
+    cat.img((err, octoUrl) => {
+      req.session.dotBind = {};
+      req.session.dotBind.username = username;
+      req.session.dotBind.access_token = access_token;
+      req.session.dotBind.img = octoUrl;
+
+      // redirect to home page
+      res.redirect('/');
+      return;
+    // if the user is not valid
+      // send back response 'user not valid'
+    }, true);
+    
+  });
 };
