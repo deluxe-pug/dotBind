0e5281154786abe48872dfa7deb403538f8b79b2
Kevin Nguyen <account@kevinwin.com>
2016-05-14 18:25:11 -0700

    Set properties on dotBind session

--- server/static/lib/regularAuth-handler.js
+++ server/static/lib/regularAuth-handler.js
@@ -1,12 +1,27 @@
+const request = require('request');
+const cat = require('octodex'); // generates a random octocat image
+
 exports.regularAuth = (req, res) => {
   // hit the nodal validating user end point
+  const email = req.body.email;
+  const password = req.body.password;
+  const username = email.split('@')[0] // map to Github email if found in database [should also be done opposite direction] or 
+
+  request({
+    uri: `http://localhost:3000/v1/access_tokens`
+  }, () => {
+
+  })
     // if the user is valid
       // set up session with username and token
-      req.session.dotBind = {};
-      req.session.dotBind.username = 'public';
-      req.session.dotBind.access_token = 'fake token';
-      // redirect to home page
-      res.redirect('/');
-    // if the user is not valid
-      // send back response 'user not valid'
+      cat.img((err, octoUrl) => {
+        req.session.dotBind = {};
+        req.session.dotBind.username = username;
+        req.session.dotBind.access_token = 'dotBind';
+        req.session.dotBind.img = octoUrl
+        // redirect to home page
+        res.redirect('/');
+      // if the user is not valid
+        // send back response 'user not valid'
+      }, true);
 };
