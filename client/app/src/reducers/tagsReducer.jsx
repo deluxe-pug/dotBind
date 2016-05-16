6fbc162a26e850688afb5ff60682f5cf4de4f774
Liam Hatcher <hatcherpiano@gmail.com>
2016-05-16 11:33:22 -0700

    Sort tags in sidebar by card_count

--- client/app/src/reducers/tagsReducer.jsx
+++ client/app/src/reducers/tagsReducer.jsx
@@ -9,11 +9,25 @@ const tagsReducer = (state = [], action) => {
       }];
 
     case 'FETCH_TAGS':
-      return [...action.payload.data.data, ...state];
+      // console.log('tags reducer payload => ', action.payload.data.data);
+      // console.log('tags reducer state => ', state);
+      // return [...action.payload.data.data, ...state];
+      return sortedTags(state, action.payload.data.data, (a, b) => a.card_count < b.card_count);
 
     default:
       return state;
   };
+};
+
+const sortedTags = (state, data, callback) => {
+  let newState = [...state, ...data];
+  console.log('before sort ===> ', newState);
+  newState.sort( (tagA, tagB) => {
+    return callback(tagA,tagB);
+  });
+
+  console.log('after sort ===> ', newState);
+  return newState;
 };
 
 export default tagsReducer;
