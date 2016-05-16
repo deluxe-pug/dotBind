71ac4cccf281cdd0c37e2c70c5b1a1a8edafc642
Kevin Nguyen <account@kevinwin.com>
2016-05-15 19:28:14 -0700

    Add user_tags endpoint

--- client/app/src/actions/endpoints.jsx
+++ client/app/src/actions/endpoints.jsx
@@ -1,6 +1,7 @@
 const endpoints = {
   cards: 'http://localhost:3000/v1/cards',
   tags: 'http://localhost:3000/v1/tags',
+  user_tags: 'http://localhost:3000/v1/user_tags',
   card_tags: 'http://localhost:3000/v1/card_tags',
   card_tags: 'http://localhost:3000/v1/card_tags',
   auth: 'http://localhost:8000/auth',
