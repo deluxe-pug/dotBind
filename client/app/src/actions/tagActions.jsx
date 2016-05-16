9d86e6e38a9eb8624b75ac474f513558bbce3169
Kevin Nguyen <account@kevinwin.com>
2016-05-15 19:29:13 -0700

    Modify axios endpoint to user_tags

--- client/app/src/actions/tagActions.jsx
+++ client/app/src/actions/tagActions.jsx
@@ -14,7 +14,9 @@ export const addTag = (tag) => {
 };
 
 export const fetchTagsAction = () => {
-  const request = axios.get(endpoints.tags);
+  const access_token = localStorage.getItem('dotBindAccessToken')
+  const request = axios.get(`${endpoints.user_tags}?access_token=${access_token}`);
+  // const request = axios.get(endpoints.tags);
   return {
     type: 'FETCH_TAGS',
     payload: request,
