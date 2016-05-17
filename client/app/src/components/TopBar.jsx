ee508f91edc065dd79ebe0274b91a8e37e5c52db
michelleheh <xiaoyu.he22@gmail.com>
2016-05-16 18:46:09 -0700

    Add buttons on topbar

--- client/app/src/components/TopBar.jsx
+++ client/app/src/components/TopBar.jsx
@@ -7,6 +7,7 @@ import SearchTagContainer from '../containers/SearchTagContainer';
 import AddCardContainer from '../containers/AddCardContainer';
 import UserProfileContainer from '../containers/UserProfileContainer';
 import { switchDisplayAction } from '../actions/searchActions';
+import { fetchCardsAction, fetchInboxAction, setToInboxAction, setToCardsAction } from '../actions/cardActions';
 
 require("../styles/topbar.css");
 
@@ -37,6 +38,16 @@ class TopBar extends React.Component {
     this.props.switchDisplay(true);
   }
 
+  handleMyCards(){
+    this.props.setToCards();
+    this.props.fetchCards();
+  }
+
+  handleInbox(){
+    this.props.setToInbox();
+    this.props.fetchInbox();
+  }
+
   render() {
     return (
       <div className='navbar-fixed topbar'>
@@ -61,6 +72,18 @@ class TopBar extends React.Component {
             </li>
 
             <li>
+              <a onClick={this.handleMyCards.bind(this)}>
+                <i className="material-icons small-icon">dashboard</i>
+              </a>
+            </li>
+
+            <li>
+              <a onClick={this.handleInbox.bind(this)}>
+                <i className="material-icons small-icon">email</i>
+              </a>
+            </li>
+
+            <li>
               <a className='dropdown-button'
                  href='#' data-beloworigin="true" data-activates='dropdown1'>
                 <i className="material-icons small-icon">more_vert</i>
@@ -81,13 +104,19 @@ class TopBar extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    search: state.search
+    cards: state.cards,
+    search: state.search,
+    cardsState: state.cardsState
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     switchDisplay: switchDisplayAction,
+    fetchInbox: fetchInboxAction,
+    fetchCards: fetchCardsAction,
+    setToInbox: setToInboxAction,
+    setToCards: setToCardsAction,
   }, dispatch);
 }
 
