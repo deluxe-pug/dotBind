2f699811955895adcdec569eaa412c02d36c78fb
Kevin Nguyen <account@kevinwin.com>
2016-05-17 11:15:37 -0700

    Remove redundant card_tags endpoint

--- client/app/src/actions/endpoints.jsx
+++ client/app/src/actions/endpoints.jsx
@@ -3,7 +3,6 @@ const endpoints = {
   tags: 'http://localhost:3000/v1/tags',
   user_tags: 'http://localhost:3000/v1/user_tags',
   card_tags: 'http://localhost:3000/v1/card_tags',
-  card_tags: 'http://localhost:3000/v1/card_tags',
   auth: 'http://localhost:8000/auth',
   search: 'http://localhost:3000/v1/search',
 };
