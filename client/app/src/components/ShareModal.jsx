0a037c391d45afc6edeb6a3673ce36b54f236cf9
Liam Hatcher <hatcherpiano@gmail.com>
2016-05-17 13:36:37 -0700

    Create form and get input value from share modal

--- client/app/src/components/ShareModal.jsx
+++ client/app/src/components/ShareModal.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 
 require('../styles/popoutform.css');
 
+let input = null;
 const ShareModal = () => (
   <div id="popup1" className="overlay">
       <div className="popup">
@@ -11,16 +12,24 @@ const ShareModal = () => (
           <a className="close" href="#">&times;</a>
           <div className="container">
             <div className="row">
+              <form onSubmit={ e => {
+                e.preventDefault();
+                if ( !input.value.trim() ) {
+                  return;
+                }
 
-              <div className="col s8">
-                <input type="text" placeholder="github handle" />
-              </div>
+                console.log(input.value);
+              }}>
+                <div className="col s8">
+                  <input type="text" ref={node => input = node} placeholder="github handle" />
+                </div>
 
-              <div className="col s4">
-                <button type="submit" className="waves-effect waves-light btn">
-                  Share!
-                </button>
-              </div>
+                <div className="col s4">
+                  <button type="submit" className="waves-effect waves-light btn">
+                    Share!
+                  </button>
+                </div>
+              </form>
 
             </div>
           </div>
