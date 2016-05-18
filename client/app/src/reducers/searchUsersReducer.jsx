af7d06841f437217e9ee9a35e82069716e703dde
michelleheh <xiaoyu.he22@gmail.com>
2016-05-18 12:21:12 -0700

    Add CLEAR_USERS	case

--- client/app/src/reducers/searchUsersReducer.jsx
+++ client/app/src/reducers/searchUsersReducer.jsx
@@ -3,6 +3,8 @@ const searchUsersReducer = (state = [], action) => {
     case 'SEARCH_USERS':
       console.log('new state: ', action.payload.data);
       return action.payload.data.data;
+    case 'CLEAR_USERS':
+      return [];
     default:
       return state;
   };
