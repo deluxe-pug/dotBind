288e6501472799963b28c7e259118586ed867a70
michelleheh <xiaoyu.he22@gmail.com>
2016-05-18 11:10:39 -0700

    Bug fix in searchUserReducer

--- client/app/src/reducers/searchUsersReducer.jsx
+++ client/app/src/reducers/searchUsersReducer.jsx
@@ -2,7 +2,7 @@ const searchUsersReducer = (state = [], action) => {
   switch(action.type) {
     case 'SEARCH_USERS':
       console.log('new state: ', action.payload.data);
-      return action.payload.data;
+      return action.payload.data.data;
     default:
       return state;
   };
