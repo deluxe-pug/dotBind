73d44ea82995cca19a8664e1163b8825d95a7d60
Liam Hatcher <hatcherpiano@gmail.com>
2016-05-17 19:18:39 -0700

    Populate language select menu

--- client/app/src/components/CardModal.jsx
+++ client/app/src/components/CardModal.jsx
@@ -17,6 +17,36 @@ import {
   saveCardFromInboxAction
 } from '../actions/cardActions';
 
+const languages = [
+  {key:0, value: 'coffee', name: 'Coffee Script'},
+  {key:1, value: 'csharp', name: 'C#'},
+  {key:2, value: 'css', name: 'CSS'},
+  {key:3, value: 'c_cpp', name: 'C++'},
+  {key:4, value: 'elixir', name: 'Elixir'},
+  {key:5, value: 'golang', name: 'Golang'},
+  {key:6, value: 'handlebars', name: 'Handlebars'},
+  {key:7, value: 'haskell', name: 'Haskell'},
+  {key:8, value: 'html', name: 'HTML'},
+  {key:9, value: 'lua', name: 'Lua'},
+  {key:10, value: 'java', name: 'Java'},
+  {key:11, value: 'json', name: 'JSON'},
+  {key:12, value: 'javascript', name: 'JavaScript'},
+  {key:13, value: 'markdown', name: 'Markdown'},
+  {key:14, value: 'matlab', name: 'Matlab'},
+  {key:15, value: 'mysql', name: 'MySQL'},
+  {key:16, value: 'objectivec', name: 'Objective-C'},
+  {key:17, value: 'php', name: 'PHP'},
+  {key:18, value: 'python', name: 'Python'},
+  {key:19, value: 'r', name: 'R'},
+  {key:20, value: 'ruby', name: 'Ruby'},
+  {key:21, value: 'rust', name: 'Rust'},
+  {key:22, value: 'sass', name: 'Sass'},
+  {key:23, value: 'scala', name: 'Scala'},
+  {key:24, value: 'typescript', name: 'Typescript'},
+  {key:25, value: 'xml', name: 'XML'}
+];
+
+
 let input;
 let editorCode = '';
 let note = '';
@@ -25,6 +55,12 @@ class CardModal extends React.Component {
     super(props);
     editorCode = this.props.code;
     note = this.props.note;
+
+    var stuff = [];
+    for ( let i = 0; i < languages.length; i++ ) {
+      stuff.push({key: i, value: languages[i]})
+    }
+    console.log(JSON.stringify(stuff));
   }
 
   componentWillMount(){
@@ -80,12 +116,12 @@ class CardModal extends React.Component {
           <div className="col s10">
             <img className="activator modal-icon" src={this.props.icon} />
             <h5>{this.props.title}</h5>
-            <div class="input-field">
-              <select className="modal-select">
-                <option value="" disabled selected>Select a language: </option>
-                <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-                <option value="3">Option 3</option>
+            <div className="input-field">
+              <select className="modal-select col s4">
+                <option value=""  >Select a language: </option>
+                {languages.map( lang => {
+                  return <option value={lang.value}>{lang.name}</option>
+                })}
               </select>
             </div>
           </div>
