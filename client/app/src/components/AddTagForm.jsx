e598aef7838a6b2c9c549a67eef234b8653d4b43
Connie Cheung <conniedcheung@gmail.com>
2016-05-18 15:13:37 -0700

    Change add-tag input-box style

--- client/app/src/components/AddTagForm.jsx
+++ client/app/src/components/AddTagForm.jsx
@@ -12,17 +12,16 @@ class AddTagForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={ (e) => {
-        e.preventDefault();
-        if ( !input.value.trim() ) {
-          return;
-        }
-        this.props.addTagToCard(input.value.toLowerCase(), this.props.user_id, this.props.id);
-        input.value = '';
+      <form className="add-tag-form"
+        onSubmit={ (e) => {
+          e.preventDefault();
+          if ( !input.value.trim() ) {
+            return;
+          }
+          this.props.addTagToCard(input.value.toLowerCase(), this.props.user_id, this.props.id);
+          input.value = '';
       }}>
-        <div className="col s4">
-          <input className="tag-input" type="text" placeholder="Add tag" ref={ node => {input = node}} />
-        </div>
+        <input className="tag-input" type="text" placeholder="Add tag" ref={ node => {input = node}} />
       </form>
     );
   }
