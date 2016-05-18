d82359f8ad407a7ba185837055412bc491047862
michelleheh <xiaoyu.he22@gmail.com>
2016-05-17 19:15:34 -0700

    Add searchUsersReducer

---
+++ client/app/src/reducers/searchUsersReducer.jsx
@@ -0,0 +1,11 @@
+const searchUsersReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'SEARCH_USERS':
+      console.log('new state: ', action.payload.data);
+      return action.payload.data;
+    default:
+      return state;
+  };
+};
+
+export default searchUsersReducer;
