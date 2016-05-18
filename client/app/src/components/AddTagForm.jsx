c46f9b7b7824345d31ac91a58eee0842484ca220
Connie Cheung <conniedcheung@gmail.com>
2016-05-18 12:37:37 -0700

    Update AddTagForm to be a class

--- client/app/src/components/AddTagForm.jsx
+++ client/app/src/components/AddTagForm.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 let input;
 
-let AddTagForm = () => (
-  <div className="col s4">
-    <input className="tag-input" type="text" placeholder="Add tag" ref={ node => {input = node}} />
-  </div>
-);
+class AddTagForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="col s4">
+        <input className="tag-input" type="text" placeholder="Add tag" ref={ node => {input = node}} />
+      </div>
+    );
+  }
+};
 
 export default AddTagForm;
