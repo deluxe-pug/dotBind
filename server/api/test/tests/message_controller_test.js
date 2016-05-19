68ba944485386ee7cdde0ac531e6dfa7a2c7e5dc
michelleheh <xiaoyu.he22@gmail.com>
2016-05-18 19:21:35 -0700

    Remove test for message endpoint

--- server/api/test/tests/message_controller_test.js
+++ server/api/test/tests/message_controller_test.js
@@ -8,24 +8,9 @@ module.exports = (() => {
 
     test(expect) {
 
-      it('Should connect to v1/messages/ endpoint as public', done => {
+      // it('Should connect to v1/messages/ endpoint as public', done => {
 
-        this.endpoint('/v1/messages?access_token=dotBind').get((status, headers, body, json) => {
-          expect(status).to.equal(200);
-          done();
-
-        });
-
-      });
-
-      // it('Should post to v1/messages/ endpoint as public', done => {
-      //   const reqBody = JSON.stringify({ from: 'public', to: 'adam', card_id: '3' });
-      //     console.log('--> this is the req body: ', reqBody);
-
-      //   this.endpoint('/v1/messages?access_token=dotBind').post('test', (status, headers, body, json) => {
-          
-      //     console.log('--> this is the res body: ', body + '');
-
+      //   this.endpoint('/v1/messages?access_token=dotBind').post((status, headers, body, json) => {
       //     expect(status).to.equal(200);
       //     done();
 
