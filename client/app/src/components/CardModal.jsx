b95bd7986fa14d766c942ab5622c67a68e0075d9
Connie Cheung <conniedcheung@gmail.com>
2016-05-18 19:02:48 -0700

    Modal looks good but is not responsive, commit before major changes

--- client/app/src/components/CardModal.jsx
+++ client/app/src/components/CardModal.jsx
@@ -95,7 +95,7 @@ class CardModal extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="modal-container">
 
         <div className="modal-nav">
           <div className="title">
@@ -121,7 +121,7 @@ class CardModal extends React.Component {
         </div>
 
         <div className="modal-editor">
-          <AceEditor height="250px" width="100%"
+          <AceEditor height="300px" width="100%"
             onFocus={this.props.remindSave.bind(this)}
             onChange={this.editorHasChanged} mode={this.state.language}
             theme="tomorrow_night" name="editor"
